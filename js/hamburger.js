8029e2f2d24809215bce0bf096c8b8faa8f6c3c1
mikeybell <michaelrobertbell@gmail.com>
2015-10-04 11:47:02 -0400

    shoes colour

--- js/hamburger.js
+++ js/hamburger.js
@@ -3,11 +3,13 @@ $(function() {
 	// jQuery Goes here!
 	$('.burger').on('click', function(){
 		$("nav").animate({left: '0'}, 250, 'swing');
+		$("body").animate({left: '30%'}, 250, 'swing');
 	});
 
 
 	$('.nav-close').on('click', function(){
-		$("nav").animate({left: '-60%'}, 250, 'swing');
+		$("nav").animate({left: '-30%'}, 250, 'swing');
+		$("body").animate({left: '0'}, 250, 'swing');
 	});
 
 
---
+++ js/redshoes.js
@@ -0,0 +1,11 @@
+$('.red-circle').mouseover(function(){
+    $('#shoes').attr('src', 'img/red-shoes.jpg');
+    });
+
+$('.yellow-circle').mouseover(function(){
+    $('#shoes').attr('src', 'img/yellow-shoes.jpg');
+    });
+
+$('.green-circle').mouseover(function(){
+    $('#shoes').attr('src', 'img/green-shoes.jpg');
+    });
